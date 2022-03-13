1e8ead4924a74919bcd29cfe173fbea372373d4b
Marko Savic <marko@minorityprogrammers.org>
2022-03-13 21:33:34 +0100

    IConfiguration initialization

---
+++ RVAPRODAVNICA.Repositories/BaseRepository.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+
+namespace RVAPRODAVNICA.Repositories
+{
+
+    public interface IBaseRepository<TEntity> 
+    {
+    }
+
+    public class BaseRepository<TEntity> : IBaseRepository<TEntity>
+    {
+
+        private IConfiguration configuration;
+
+        private BaseRepository(IConfiguration configuration) { 
+        
+            this.configuration = configuration;
+        }
+
+
+    }
+}
