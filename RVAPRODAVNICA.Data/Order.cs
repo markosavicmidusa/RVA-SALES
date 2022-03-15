ea567bb9589b16c23978bb5631bfbd95f2360697
Marko Savic <marko@minorityprogrammers.org>
2022-03-15 19:02:23 +0100

    adding data anotaitions

--- RVAPRODAVNICA.Data/Order.cs
+++ RVAPRODAVNICA.Data/Order.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace RVAPRODAVNICA.Data
 {
+    [Table("orders")]
     public class Order : Base
     {
     }
--- RVAPRODAVNICA.Data/Product.cs
+++ RVAPRODAVNICA.Data/Product.cs
@@ -1,12 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace RVAPRODAVNICA.Data
 {
+    [Table("products")]
     public class Product : Base
     {
+
+        [Column("name")]
+        public string? name { get; set; }
     }
 }
