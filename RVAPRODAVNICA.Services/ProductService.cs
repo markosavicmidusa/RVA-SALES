140df94d57406fb0ae64e3a7453a489b6ba5dbf8
Marko Savic <marko@minorityprogrammers.org>
2022-03-15 22:13:57 +0100

    comments

--- RVAPRODAVNICA.Services/ProductService.cs
+++ RVAPRODAVNICA.Services/ProductService.cs
@@ -29,30 +29,47 @@ namespace RVAPRODAVNICA.Services
             this.productRepository = productRepository;
         
         }
-
+        /// <summary>
+        /// CREATE
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <returns></returns>
 
         public int Create(Product obj)
         {
             return productRepository.Create(obj);
         }
-
+        /// <summary>
+        /// DELETE
+        /// </summary>
+        /// <param name="obj"></param>
         public void Delete(Product obj)
         {
            productRepository.Delete(obj);
         }
-
+        /// <summary>
+        /// Get by ID
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
         public Product Get(int id)
         {
             return productRepository.readOne(id);
             
         }
-
+        /// <summary>
+        /// Read all
+        /// </summary>
+        /// <returns></returns>
         public List<Product> ReadAll()
         {
           return productRepository.readAll();
             
         }
-
+        /// <summary>
+        /// UPDATE
+        /// </summary>
+        /// <param name="obj"></param>
         public void Update(Product obj)
         {
             productRepository.Update(obj);
