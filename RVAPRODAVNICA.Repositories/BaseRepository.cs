3d77d6d0af5f3b974bc78eae8e0dba3c127ef8a3
Marko Savic <marko@minorityprogrammers.org>
2022-03-15 20:08:13 +0100

    adding crud functionalities

--- RVAPRODAVNICA.Repositories/BaseRepository.cs
+++ RVAPRODAVNICA.Repositories/BaseRepository.cs
@@ -13,6 +13,13 @@ namespace RVAPRODAVNICA.Repositories
     public interface IBaseRepository<TEntity> 
     {
         List<TEntity> readAll();
+        TEntity readOne(int id);
+
+        int Create(TEntity obj);
+        void Update(TEntity obj);
+        void Delete(TEntity obj);
+
+
     }
 
     public class BaseRepository<TEntity> : IBaseRepository<TEntity>
@@ -20,12 +27,14 @@ namespace RVAPRODAVNICA.Repositories
 
         private IConfiguration configuration;
         public string connectionString;
+        public MySqlConnection connection;
 
         public BaseRepository(IConfiguration configuration) {
 
             SimpleCRUD.SetDialect(SimpleCRUD.Dialect.MySQL);
             this.configuration = configuration;
             connectionString = configuration.GetConnectionString("DefaultConnection");
+            connection =new MySqlConnection(connectionString);
 
         }
         /// <summary>
@@ -34,10 +43,52 @@ namespace RVAPRODAVNICA.Repositories
         /// <returns></returns>
         public List<TEntity> readAll()
         {
-            var connection = new MySqlConnection(connectionString);
+
             List<TEntity> results = connection.GetList<TEntity>().ToList();
             return results;
 
+        }
+        /// <summary>
+        /// Get one record 
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        /// <exception cref="NotImplementedException"></exception>
+        public TEntity readOne(int id)
+        {
+
+            TEntity result = connection.Get<TEntity>(id);
+            return result;
+
+        }
+        /// <summary>
+        /// Create
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <returns></returns>
+        /// <exception cref="NotImplementedException"></exception>
+        public int Create(TEntity obj)
+        {
+            var result = connection.Insert(obj);
+            return (int)result;
+        }
+        /// <summary>
+        /// Update
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <exception cref="NotImplementedException"></exception>
+        public void Update(TEntity obj)
+        {
+            connection.Update(obj);
+        }
+        /// <summary>
+        /// Delete
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <exception cref="NotImplementedException"></exception>
+        public void Delete(TEntity obj)
+        {
+           connection.Delete(obj);
         }
     }
 }
