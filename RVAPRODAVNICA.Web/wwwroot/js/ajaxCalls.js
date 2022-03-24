bcd8b21d13f3219e10987d92b658ecef6c85863b
Marko Savic <marko@minorityprogrammers.org>
2022-03-24 23:50:12 +0100

    adding ajax request

---
+++ RVAPRODAVNICA.Web/wwwroot/js/ajaxCalls.js
@@ -0,0 +1,22 @@
+﻿function TableSearch(dataObject, url, pageNumber, rowsPerPage) {
+
+    var data = { "pageNumber": pageNumber, "rowsPerPage": rowsPerPage }
+
+    $.ajax({
+
+        method: "GET",
+        url: url,
+        data: data,
+        success: function (response) {
+            var len = $(response).find('.counter-rows').length;
+            //alert(len)
+            if (len != null && len > 0) {
+                dataObject.append(response);
+                $(".row-helper").fadeIn("slow");
+            }
+        },
+        error: function () {
+            //alert(errorText)
+        }
+    })
+}
